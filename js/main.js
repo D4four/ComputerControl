9aff3bb3999657649e462649c4bef8092162767f
unknown <danil.konovalov.70@gmail.com>
2024-10-15 19:34:45 +0300

    pochti

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ $(function() {
         $this.toggleClass("active")
     })
 
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active')
+    })
+
+    $('[data-fancybox]').fancybox({
+        youtube : {
+            controls : 1,
+            showinfo : 1
+        }
+    })
+
 }) 
